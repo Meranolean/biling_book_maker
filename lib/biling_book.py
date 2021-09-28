558fff2aa455d50cfa5729bd848e813853bd6dc3
Magnus1907 <magnus1907@yandex.ru>
2021-09-28 22:25:38 +0300

    End first implementation

---
+++ lib/biling_book.py
@@ -0,0 +1,30 @@
+from blb_helpers import print_book
+from blb_func import parse_study_english_words
+
+BEGIN = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:l="http://www.w3.org/1999/xlink">
+  <description>
+    <title-info>
+        <genre>antique</genre>
+        <author><first-name></first-name><last-name>{}</last-name></author>
+        <book-title>{}</book-title>
+        <!-- <coverpage><image l:href="#img_0"/></coverpage> -->
+        <lang>es</lang>
+    </title-info>
+  </description>
+<body>'''
+
+
+END = '''
+</body>
+</FictionBook>
+'''
+
+import json
+def make_book(name, author, url, npage):
+    # book = parse_study_english_words(url, npage)
+    book = json.load(open('../../../books_to_print'))[1]
+    book_output = print_book(book, name, BEGIN.format(author, name), END)
+    with open(name.lower().replace(' ', '_') + '.fb2', 'w') as fout:
+        fout.write(book_output)
--- lib/blb_helpers.py
+++ lib/blb_helpers.py
@@ -1,4 +1,9 @@
-def clean_string(string):
+d0 = '\n'
+d1 = '\n  '
+d2 = '\n    '
+d3 = '\n      '
+
+def normalize(string):
     string = string.replace('\r', '').replace('\n', '')
     while string[0] == ' ':
         string = string[1: ]
@@ -7,3 +12,67 @@ def clean_string(string):
     return string
 
 
+def find_chapters(book, begin, end):
+    chapters_id = []
+    for i, (eng, rus) in enumerate(book[begin: end]):
+        norm_eng = normalize(eng)
+        if norm_eng.startswith('CHAPTER') or ('CHAPTER' in norm_eng):
+            chapters_id.append(begin + i)
+    chapters_id.append(end)
+    return chapters_id
+
+def find_parts(book):
+    part_id = []
+    for i, (eng, rus) in enumerate(book):
+        norm_eng = normalize(eng)
+        if norm_eng.startswith('Part'):
+            part_id.append(i)
+    part_id.append(len(book))
+    return part_id
+
+
+def print_chapter_table(book, begin, end, string):
+    string += d0 + '<table>'
+    for j in range(begin, end):
+        eng, rus = book[j]
+        string += d1 + '<tr>'
+        string += d2 + '<td>' + normalize(eng) + '</td>'
+        string += d2 + '<td>' + normalize(rus) + '</td>'
+        string += d1 + '</tr>'
+    string += d0 + '</table>'
+
+
+def print_chapter(book, begin, end, string):
+    for j in range(begin, end):
+        eng, rus = book[j]
+        string += d1 + '<p>' + normalize(eng) + '</p>'
+        string += d1 + '<p>' + normalize(rus) + '</p>'
+        string += d1 + '=' * 10
+    return string
+
+
+def print_part(book, begin, end, string):
+    chapter_to_write = find_chapters(book, begin, end)
+    for chapter_begin, chapter_end in zip(chapter_to_write[: -1], chapter_to_write[1:]):
+        string += d0 + '<section>' + d1 + '<title>' + d2 + '<p>' + \
+                  normalize(book[chapter_begin][0]) + '</p>' + \
+                  d1 + '</title>' + d1 + '<empty-line/>'
+        print(normalize(book[chapter_begin][0]))
+        string = print_chapter(book, chapter_begin + 1, chapter_end, string)
+        string += d0 + '</section>'
+    return string
+
+
+def print_book(book, book_name, BEGIN, END):
+    string = BEGIN
+    string += d1 + '<title>' + d2 + '<p><strong>' + book_name + '</strong></p>' + d1 + '</title>'
+    
+    parts_to_write = find_parts(book)
+    for part_begin, part_end in zip(parts_to_write[: -1], parts_to_write[1:]):
+        string += d0 + '<section>' + d1 + '<title>' + d2 + '<p><strong>' + \
+                  normalize(book[part_begin][0]) + '</strong></p>' + \
+                  d1 + '</title>'
+        string = print_part(book, part_begin + 1, part_end, string)
+        string += d0 + '</section>'
+    string += END
+    return string
